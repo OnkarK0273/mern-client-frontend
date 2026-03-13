d774da2e4d77bf0bc094e1ff7240bcc2e8d94483
Onkar k <onkarok0273.1@gmail.com>
2026-03-13 20:57:49 +0530

    feat: add tenantid on refresh select

--- src/components/custom/tenant-select.tsx
+++ src/components/custom/tenant-select.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { Tenant } from "@/lib/types";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import {
   Select,
   SelectContent,
@@ -9,20 +9,23 @@ import {
   SelectValue,
 } from "../ui/select";
 const TenantSelect = ({ restaurants }: { restaurants: { data: Tenant[] } }) => {
+  const searchParams = useSearchParams();
   const router = useRouter();
   const handleSelect = (id: string) => {
     router.push(`?restaurantId=${id}`);
   };
 
+  const tenantId = searchParams.get("restaurantId");
+
   return (
-    <Select onValueChange={handleSelect}>
+    <Select onValueChange={handleSelect} defaultValue={tenantId || ""}>
       <SelectTrigger className="w-[180px]">
         <SelectValue placeholder="Select Restaurant" />
       </SelectTrigger>
       <SelectContent>
         {restaurants.data.map((restaurant) => {
           return (
-            <SelectItem key={restaurant.id} value={restaurant.id}>
+            <SelectItem key={restaurant.id} value={String(restaurant.id)}>
               {restaurant.name}
             </SelectItem>
           );
