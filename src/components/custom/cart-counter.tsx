31074b782cb817aab95f0a1873fd9d67454b7cc4
Onkar k <onkarok0273.1@gmail.com>
2026-03-27 11:57:26 +0530

    feat: fix renamed serchparams

--- src/components/custom/cart-counter.tsx
+++ src/components/custom/cart-counter.tsx
@@ -12,7 +12,7 @@ const CartCounter = () => {
   return (
     <>
       <div className="relative">
-        <Link href={`/cart?tenantId=${tenantId}`}>
+        <Link href={`/cart?restaurantId=${tenantId}`}>
           <ShoppingBasket className="hover:text-primary" />
         </Link>
         <span className="absolute -top-4 -right-5 h-6 w-6 flex items-center justify-center rounded-full bg-primary font-bold text-white">
